84d7bf4accb870498f27aee75a55dc3b19b36a90
alextalijan <acatalijan@icloud.com>
2025-10-05 19:48:46 -0500

    feat: add option to change the folder of a file on its page

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -9,8 +9,48 @@ module.exports = {
         name: req.params.fileName,
         userId: req.user.id,
       },
+      include: {
+        folder: true,
+      },
+    });
+    const folders = await prisma.folder.findMany({
+      where: {
+        userId: req.user.id,
+      },
     });
 
-    res.render('file', { file });
+    res.render('file', { file, folders });
+  },
+  changeFolder: async (req, res) => {
+    let folderId;
+    if (req.body.folder === 'none') {
+      folderId = null;
+    } else {
+      folderId = req.body.folder;
+    }
+
+    // Change the folder a file belongs in
+    await prisma.file.updateMany({
+      where: {
+        name: req.params.itemName,
+        userId: req.user.id,
+      },
+      data: {
+        folderId,
+      },
+    });
+
+    // If the file has been moved to a folder, redirect there, else home page
+    if (folderId) {
+      const folder = await prisma.folder.findUnique({
+        where: {
+          id: folderId,
+        },
+      });
+
+      res.redirect(`/folders/${folder.name}`);
+    } else {
+      res.redirect('/');
+    }
   },
 };
--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -4,6 +4,7 @@ const isLoggedIn = require('../utils/isLoggedIn');
 
 const router = Router();
 
-router.get('/:fileName', controller.fileGet);
+router.get('/:fileName', isLoggedIn, controller.fileGet);
+router.post('/:fileName/change-folder', isLoggedIn, controller.changeFolder);
 
 module.exports = router;
